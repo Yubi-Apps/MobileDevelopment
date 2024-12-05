1e45f57e623e8b070d92aa6fed992dc451b0a454
MuhamadFahmi284 <114407930+MuhamadFahmi284@users.noreply.github.com>
2024-12-05 15:44:41 +0700

    splashscreen,onboarding,home,article,detailarticle

---
+++ app/src/main/java/com/dicoding/yubi_apps/ui/analyze/AnalyzeFragment.kt
@@ -0,0 +1,13 @@
+package com.dicoding.yubi_apps.ui.analyze
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dicoding.yubi_apps.R
+
+
+class AnalyzeFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/dicoding/yubi_apps/ui/history/HistoryFragment.kt
@@ -0,0 +1,12 @@
+package com.dicoding.yubi_apps.ui.history
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dicoding.yubi_apps.R
+
+class HistoryFragment : Fragment() {
+
+}
